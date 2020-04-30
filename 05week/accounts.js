572ed2d5d33735773202ae126b6e72b77f40e60a
evantwidwell <evanski@gmail.com>
2020-04-30 14:25:44 -0500

    accounts assignment

--- 05week/accounts.js
+++ 05week/accounts.js
@@ -5,41 +5,50 @@ class Account {
     this.accountNumber = accountNumber;
     this.owner = owner;
     this.transactions = [];
-    this.balance = 0;
+    // this.balance = 0;
   }
-    
+  getBalance(){
+    // console.log(`Current transactions  ${this.transactions}`);
+    let balance = 0;
+    let newTransactions = this.transactions
+    if(newTransactions.length == 0){
+      return balance
+    } else{
+    for (let i = 0; i < this.transactions.length ; i++){
+      balance = balance + newTransactions[i].amount 
+      // console.log(newTransactions[i])
+      // console.log(`transaction at ${i} is ${newTransactions[i].amount} to ${newTransactions[i].payee}`)
+      // console.log(balance)
+    }
+    return balance
+    }
+  }
 
     deposit(amount){
       if(amount > 0){
-      console.log("new deposit amount " + amount)
+      // console.log("new deposit amount " + amount)
       let payee = "deposit"
-      this.balance += amount
+      // this.balance += amount
       this.transactions.push(new transaction(amount, payee))
+      // console.log(this.transactions.amount)
+      
       } else{
         console.log("This is not a valid transaction")
       }
     }
-      debit(payee, amount){
-        if(this.balance - amount < 0){
-          console.log("This is not a valid transaction")
+      debit(amount, payee){
+        // console.log(`Current balance is ${this.getBalance()}`)
+        // console.log(`After next transactions, balance is ${this.getBalance() + amount}`)
+        if((this.getBalance() + amount) < 0){
+          // console.log(this.getBalance())
+          console.log("This is not a valid transaction, you are overdrawn")
         } else {
-          this.balance = this.balance - amount
           this.transactions.push(new transaction(amount, payee))
-          // console.log(this.transactions)
         }
       }
-      getBalance(){
-        console.log(`Current balance is ${this.balance}`);
-        // let balance = 0;
-        // for (let i =0; i <= this.transactions.length; i++){
-        //   // console.log(this.transactions[i][amount])
-        //   console.log(balance)
-        //   // balance += this.transactions[i][amount]
-        //   // console.log(balance)
-        //   console.log(this.transactions.length)
-        // }
-      // }
-    }
+      
+       
+    
 }
   class transaction{
     constructor(amount, payee){
@@ -56,6 +65,10 @@ console.log(acct1.owner); // John Doe
 console.log(acct1.getBalance()); // 0
 acct1.deposit(100)
 acct1.deposit(100)
+console.log(acct1.getBalance()); //200
+acct1.debit(-100, "HEB")
+console.log(acct1.getBalance()); // 100
+acct1.debit(-200, "HEB") //not allowed
 console.log(acct1.getBalance()); // 100
 
 acct1.deposit(-200)  // should not be allowed
