ad92ce136b12f8a237e83a6ea00f234deb71ac10
evantwidwell <evanski@gmail.com>
2020-05-07 20:53:58 -0500

    fetch practice

--- 07week/fetch1.js
+++ 07week/fetch1.js
@@ -3,21 +3,35 @@ let arrayOfPosts;
 // this function waits for the web page to be loaded, when it does it will run the code inside of it which happen to be getPosts()
 window.onload = function() {
   getPosts()
+  console.log("window loaded")
+  
 
 }
 
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
+
+let x = 8
+
 const getPosts = () => {
-  fetch('http://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(posts => arrayOfPosts = posts)
+  fetch(`http://jsonplaceholder.typicode.com/posts?_limit=${x}`)
+  .then(res => {
+    if(!res.ok) {
+      throw Error(res.statusText)
+    } console.log(res)
+    return res.json()
+    
+  })
+  .then(posts => arrayOfPosts = posts)
+  .catch(err => 
+  console.log(`Error,  ${err}`))
+  
 }
 
 // this function logs the results in your browsers console
 const consolePosts = () => {
   console.log(arrayOfPosts)
 }
-
+// consolePosts()
 // this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
 const displayPost = () => {
   const allPosts = document.getElementById('all-posts')
